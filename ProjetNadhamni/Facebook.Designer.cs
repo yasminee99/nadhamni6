2cf6a6ce8f64c229954b97c9c1fbe2ecfc735413
yasminee99 <64501536+yasminee99@users.noreply.github.com>
2020-04-29 02:50:31 +0200

    Add files via upload

---
+++ ProjetNadhamni/Facebook.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace ProjetNadhamni
+{
+    partial class Facebook
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_signIn = new System.Windows.Forms.Button();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.txt_password = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.txt_username = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pic_fb = new System.Windows.Forms.PictureBox();
+            this.pic_google = new System.Windows.Forms.PictureBox();
+            this.pic_twitter = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_fb)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_google)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_twitter)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_signIn
+            // 
+            this.btn_signIn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(163)))), ((int)(((byte)(194)))));
+            this.btn_signIn.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_signIn.ForeColor = System.Drawing.Color.White;
+            this.btn_signIn.Location = new System.Drawing.Point(46, 267);
+            this.btn_signIn.Name = "btn_signIn";
+            this.btn_signIn.Size = new System.Drawing.Size(210, 36);
+            this.btn_signIn.TabIndex = 24;
+            this.btn_signIn.Text = "Sign In with facebook";
+            this.btn_signIn.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::ProjetNadhamni.Properties.Resources.password__1_;
+            this.pictureBox3.Location = new System.Drawing.Point(46, 197);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(24, 24);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 22;
+            this.pictureBox3.TabStop = false;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.White;
+            this.panel2.Location = new System.Drawing.Point(46, 227);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(200, 1);
+            this.panel2.TabIndex = 20;
+            // 
+            // txt_password
+            // 
+            this.txt_password.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(93)))), ((int)(((byte)(173)))), ((int)(((byte)(226)))));
+            this.txt_password.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txt_password.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_password.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.txt_password.Location = new System.Drawing.Point(76, 206);
+            this.txt_password.Name = "txt_password";
+            this.txt_password.Size = new System.Drawing.Size(180, 15);
+            this.txt_password.TabIndex = 18;
+            this.txt_password.Text = "Password";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Location = new System.Drawing.Point(46, 186);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 1);
+            this.panel1.TabIndex = 17;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::ProjetNadhamni.Properties.Resources.user__1_;
+            this.pictureBox2.Location = new System.Drawing.Point(46, 156);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(24, 24);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 16;
+            this.pictureBox2.TabStop = false;
+            // 
+            // txt_username
+            // 
+            this.txt_username.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(93)))), ((int)(((byte)(173)))), ((int)(((byte)(226)))));
+            this.txt_username.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txt_username.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_username.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.txt_username.Location = new System.Drawing.Point(76, 165);
+            this.txt_username.Name = "txt_username";
+            this.txt_username.Size = new System.Drawing.Size(180, 15);
+            this.txt_username.TabIndex = 15;
+            this.txt_username.Text = "Username";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(106, 89);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 16);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "NADHAMNI";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::ProjetNadhamni.Properties.Resources.calendar__1_;
+            this.pictureBox1.Location = new System.Drawing.Point(109, 21);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(65, 65);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 13;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(118, 342);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 13);
+            this.label2.TabIndex = 28;
+            this.label2.Text = "Login with ?";
+            // 
+            // pic_fb
+            // 
+            this.pic_fb.Image = global::ProjetNadhamni.Properties.Resources.facebook__4_;
+            this.pic_fb.Location = new System.Drawing.Point(76, 368);
+            this.pic_fb.Name = "pic_fb";
+            this.pic_fb.Size = new System.Drawing.Size(40, 40);
+            this.pic_fb.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic_fb.TabIndex = 27;
+            this.pic_fb.TabStop = false;
+            // 
+            // pic_google
+            // 
+            this.pic_google.Image = global::ProjetNadhamni.Properties.Resources.google_plus;
+            this.pic_google.Location = new System.Drawing.Point(124, 368);
+            this.pic_google.Name = "pic_google";
+            this.pic_google.Size = new System.Drawing.Size(40, 40);
+            this.pic_google.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic_google.TabIndex = 26;
+            this.pic_google.TabStop = false;
+            // 
+            // pic_twitter
+            // 
+            this.pic_twitter.Image = global::ProjetNadhamni.Properties.Resources.twitter;
+            this.pic_twitter.Location = new System.Drawing.Point(170, 368);
+            this.pic_twitter.Name = "pic_twitter";
+            this.pic_twitter.Size = new System.Drawing.Size(40, 40);
+            this.pic_twitter.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic_twitter.TabIndex = 25;
+            this.pic_twitter.TabStop = false;
+            // 
+            // Facebook
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(93)))), ((int)(((byte)(173)))), ((int)(((byte)(226)))));
+            this.ClientSize = new System.Drawing.Size(305, 446);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pic_fb);
+            this.Controls.Add(this.pic_google);
+            this.Controls.Add(this.pic_twitter);
+            this.Controls.Add(this.btn_signIn);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.txt_password);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.txt_username);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Facebook";
+            this.Text = "Facebook";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_fb)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_google)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_twitter)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_signIn;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox txt_password;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.TextBox txt_username;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pic_fb;
+        private System.Windows.Forms.PictureBox pic_google;
+        private System.Windows.Forms.PictureBox pic_twitter;
+    }
+}
